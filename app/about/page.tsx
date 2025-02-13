93c95ecda3c1a2f4409cf7f505fd507d3c382f7a
Luciano Figueroa <lumusitech@gmail.com>
2025-02-13 00:12:27 -0300

    feat: add base pages

---
+++ app/about/page.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from "next"
+
+ export const metadata: Metadata = {
+  title: "About page",
+  description: "This is the About page",
+  keywords: ["about page", "Lumusitech", "información"],
+}
+
+export default function AboutPage(){
+  return (
+    <>
+    <span className="text-5xl">About page</span>
+    <p>{new Date().toDateString()}</p>
+    </>
+  )
+
+}
---
+++ app/contact/page.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Contact page",
+  description: "This is the Contact page",
+  keywords: ["contact page", "Lumusitech", "contacto"],
+}
+
+export default function ContactPage(){
+  return (
+    <>
+    <span className="text-5xl">Contact page</span>
+    <p>{new Date().toDateString()}</p>
+    </>
+  )
+
+}
