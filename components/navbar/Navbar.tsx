09fad3ba85c8293e717d3921ab2b99b973ca439e
Luciano Figueroa <lumusitech@gmail.com>
2025-02-13 01:45:25 -0300

    feat: add next links for nav

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,19 +1,23 @@
+import Link from 'next/link'
+
 export const Navbar = () => {
   return (
     <nav className='flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded'>
-      <span>Home</span>
+      <Link href='/'>
+        <span>Home</span>
+      </Link>
 
       <div className='flex flex-1'></div>
 
-      <a className='mr-2' href='/about'>
+      <Link className='mr-2' href='/about'>
         About
-      </a>
-      <a className='mr-2' href='/pricing'>
+      </Link>
+      <Link className='mr-2' href='/pricing'>
         Pricing
-      </a>
-      <a className='mr-2' href='/contact'>
+      </Link>
+      <Link className='mr-2' href='/contact'>
         Contact
-      </a>
+      </Link>
     </nav>
   )
 }
